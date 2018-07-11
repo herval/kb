877b491e593b1229737cb7f6178d28edb7771e2a
herval <hervalfreire@gmail.com>
2018-07-10 23:05:59 -0700

    building native binaries ALMOST works

--- build-native.sh
+++ build-native.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-$GRAALVM_HOME/bin/native-image -cp ./build/libs/kb.jar -H:Name=kb -H:Class=kb.MainKt -H:+ReportUnsupportedElementsAtRuntime
+set -ex
+
+gradle shadowJar
+
+$GRAALVM_HOME/bin/native-image -cp ./build/libs/kb.jar -H:Name=kb -H:Class=kb.MainKt -H:+ReportUnsupportedElementsAtRuntime -H:IncludeResources='./src/main/resources/*' -H:IncludeResources='./src/main/resources/templates/*'
